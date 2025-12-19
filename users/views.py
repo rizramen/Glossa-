43ab4b83a5122777994fc67ea6aad6538243da18
@s0596226 <fariz.rahman@student.htw-berlin.de>
2025-12-19 13:06:39 +0100

    Added first App users

---
+++ users/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic import TemplateView
+
+class LoginView(TemplateView):
+    template_name = "users/login.html"
+
+class RegisterView(TemplateView):
+    template_name = "users/register.html"
